9fec63b59abe3ff5e7b6a81c24e9141c5afd9495
LlamaOnDrugs <47127170+LlamaOnDrugs@users.noreply.github.com>
2019-06-15 14:14:03 +0100

    Update dbtest.py

--- bin/dbtest.py
+++ bin/dbtest.py
@@ -9,18 +9,18 @@ import config
 from models import Superblock, Proposal, GovernanceObject, Setting, Signal, Vote, Outcome, Watchdog
 from models import VoteSignals, VoteOutcomes
 from peewee import PeeweeException  # , OperationalError, IntegrityError
-from terracoind import TerracoinDaemon
-import terracoinlib
+from QuantisNetd import QuantisNetDaemon
+import QuantisNetlib
 from decimal import Decimal
-terracoind = TerracoinDaemon.from_terracoin_conf(config.terracoin_conf)
+QuantisNetd = QuantisNetDaemon.from_QuantisNet_conf(config.QuantisNet_conf)
 import misc
 # ==============================================================================
 # do stuff here
 
 pr = Proposal(
     name='proposal7',
-    url='https://terracoincentral.com/proposal7',
-    payment_address='yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV',
+    url='https://testproposal.com/proposal7',
+    payment_address='QM1sn2HmMcuVWtRXLbYP8RvwsZfGCK9tD1',
     payment_amount=39.23,
     start_epoch=1483250400,
     end_epoch=1491022800,
@@ -28,18 +28,18 @@ pr = Proposal(
 
 # sb = Superblock(
 #     event_block_height = 62500,
-#     payment_addresses = "yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui|yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV",
+#     payment_addresses = "QM1sn2HmMcuVWtRXLbYP8RvwsZfGCK9tD1|QMFF2uEjWCRMS8U8pjjcNQXxaHgHJhm5wL",
 #     payment_amounts  = "5|3"
 # )
 
 
-# TODO: make this a test, mock 'terracoind' and tie a test block height to a
+# TODO: make this a test, mock 'QuantisNetd' and tie a test block height to a
 # timestamp, ensure only unit testing a within_window method
 #
 # also, create the `within_window` or similar method & use that.
 #
 bh = 131112
-bh_epoch = terracoind.block_height_to_epoch(bh)
+bh_epoch = QuantisNetd.block_height_to_epoch(bh)
 
 fudge = 72000
 window_start = 1483689082 - fudge
@@ -56,7 +56,7 @@ else:
     print("Within window, we're good!")
 
 # pdb.set_trace()
-# terracoind.get_object_list()
+# QuantisNetd.get_object_list()
 # ==============================================================================
 # pdb.set_trace()
 1
